49aae82d7cb2527f0cbee0886744d3a8e805b157
momoladebrouill <momoladebrouill@gmail.com>
2023-05-04 18:21:35 +0200

    dunyfied

--- Marc/huffman/mp2zip.ml
+++ Marc/huffman/mp2zip.ml
@@ -1,3 +1,8 @@
-open huffman
 
-construit_code [|1;2;3|]
+
+let () =
+    let s = Sys.argv.(1) in
+    let a,l = Compression.compresse s in
+    Printf.printf "avant : %s\npendant : " s;
+    List.iter (fun x -> print_char (if x then '1' else '0')) l;
+    Printf.printf "\napres : %s\n" (Decompression.decompress a l)
