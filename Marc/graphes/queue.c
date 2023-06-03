2c7b343f72ecc574c2bbe9251db9a849ba885a11
momoladebrouill <momoladebrouill@gmail.com>
2023-06-03 21:42:46 +0200

    queueu c'est ok lesogooog

--- Marc/graphes/queue.c
+++ Marc/graphes/queue.c
@@ -1,3 +1,7 @@
+#define MAXV 10
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdbool.h>
 struct queue {
     int elts[MAXV];
     int front;
@@ -11,32 +15,47 @@ struct stack {
 };
 typedef struct stack stack;
 
-void init_stack(stack *s)
-{
+void init_stack(stack *s){
     s -> back = -1;
 }
-
-int pop(stack *s)
-{
+int pop(stack *s){
     return s->elts[s->back--];
 }
-
-void push(stack *s, int x)
-{
-    s->elts[++s->back] = x;
+void push(stack *s, int x){
+    s->elts[++(s->back)] = x;
+}
+bool empty_s(stack *s){
+	return s->back==-1;
 }
 
-void init_queue(queue *s)
-{
-    // initialise la file
+void init_queue(queue *s){
+    s->back = 0;
+    s->front = -1;
 }
 
 int dequeue(queue *s)
 {
-    // defile un élément
+    int v = s->elts[s->back];
+    s->back = (s->back+MAXV+1)%MAXV;
+    return v;
 }
 
 void enqueue(queue *s, int x)
 {
-    // enfile x sur la file s
+    s->front = (s->front+1)%MAXV;
+    s->elts[s->front] = x;
+}
+bool empty_q(queue *s){
+	return (s->back - s->front+MAXV-1)%MAXV==0;
+}
+
+int main(void){
+	queue * s = malloc(sizeof(queue));
+	init_queue(s);
+	for(int i=0;i<MAXV/2;i++){enqueue(s,i);printf("enquue %d.\n",i);}
+	printf("elements : ");
+	for(int i=0;i<MAXV/2;i++)printf("%d ",s->elts[i]);
+	printf("\ndeque : ");
+	for(int i=0;!empty_q(s);i++)printf("%d ",dequeue(s));
+	printf("\n");
 }
